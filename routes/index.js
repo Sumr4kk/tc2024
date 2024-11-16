cc08e3ac9c3d880645f81c00f3bb5228ff9d78d7
Sumr4kk <alimbeckir@yandex.ru>
2024-11-16 12:19:58 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Березы*/
+router.get('/bereza', function(req, res, next) {
+  res.send("<h1>Страница Березы</h1>")
+});
+
+/* Страница Дуба*/
+router.get('/dub', function(req, res, next) {
+  res.send("<h1>Страница Дуба</h1>")
+});
+
+/* Страница Сосны*/
+router.get('/sosna', function(req, res, next) {
+  res.send("<h1>Страница Сосны</h1>")
+});
+
 module.exports = router;
