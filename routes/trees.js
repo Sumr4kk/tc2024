ba435d2aada82be4e56970a9027e5503dcb600a7
Sumr4kk <alimbeckir@yandex.ru>
2024-11-16 15:55:15 +0300

    tc_8_3

--- routes/trees.js
+++ routes/trees.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Tree = require('../models/tree').Tree;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница деревьев */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var trees = await Tree.find({nick: req.params.nick});
+    console.log(trees)
+    if(!trees.length) return next(new Error("Нет такого дерева в лесу"))
+    var tree = trees[0];
+    res.render('tree', {
+        title: tree.title,
+        picture: tree.avatar,
+        desc: tree.desc
+    })
 });
 
 module.exports = router;
