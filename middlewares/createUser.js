9a814b8ceb8a7ce834057bf5b62cf4225a68155f
Sumr4kk <alimbeckir@yandex.ru>
2024-12-17 19:55:13 +0300

    tc_10_6

--- middlewares/createUser.js
+++ middlewares/createUser.js
@@ -1,9 +1,11 @@
 var User = require("../models/user").User;
+
 module.exports = async function(req,res,next){
-res.locals.user = null
-var users = await User.findById(req.session.user_id);
-if (users.length != 0) {
-    res.locals.user = users[0];
-}
-next();
+    res.locals.user = null
+
+    var user = await User.findById(req.session.user_id);
+    if (user != null) {
+        res.locals.user = user[0];
+    }
+    next();
 }
